91962a16cd8769b61ed71a7d3cf01fbe90841a72
assemer <102959350+assemer@users.noreply.github.com>
2022-05-29 20:03:30 +0000

    Add files via upload

---
+++ assets/inc/main.php
@@ -0,0 +1,24 @@
+<?php
+/*
+class Product {
+	function DB()
+	{
+		$sql = mysqli_connect("127.0.0.1","root","","NovaShop");
+		if ($sql->connect_error) {
+			die("Connection failed: " . $sql->connect_error);
+		}
+		return $sql;
+	}
+	public function GetProducte()
+	{
+		$sql = "SELECT * FROM products";
+		$req = mysqli_query($this->DB(), $sql);
+		return $req;
+	}
+
+
+}
+$products = new Product();
+$products1111 = $products->GetProducte();
+*/
+?>
